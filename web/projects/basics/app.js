70d29303df46d8ee1bd9516c9591f69904466fc9
Bri Hassall <hassallbrianna@gmail.com>
2025-10-28 21:31:00 +1100

    Week 1 prac code from videos

--- web/projects/basics/app.js
+++ web/projects/basics/app.js
@@ -1,1 +1,36 @@
-console.log('Hello World');
+
+let experiment = 'Stroop';
+
+let welcomeMessage = `
+Welcome to our ` + experiment + ` experiment!
+Please read the instructions carefully.
+Welcome to our ` + experiment + ` experiment!
+Please read the instructions carefully.
+Welcome to our ` + experiment + ` experiment!
+Please read the instructions carefully.
+`;
+
+// console.log(welcomeMessage);
+// console.log(typeof experiment); // string
+// console.log(experiment.charAt(0)); // 'S'
+
+let trialCountMax = 20;
+// console.log(typeof trialCountMax); // number
+
+let colours = ['red', 'green', 'blue'];
+// TODO: randomise colours
+
+// alert('Welcome to the ' + experiment + ' experiment!');
+
+trialCountMax = 40;
+
+// At the halfway point we will display a pause screen
+let halfWayCount = trialCountMax / 2;
+
+// console.log(halfWayCount); // Expected: 20
+
+let correct = false;
+console.log(typeof correct);
+
+console.log(10 > 15);
+
